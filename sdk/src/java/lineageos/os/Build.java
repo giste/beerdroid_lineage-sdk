PalyrimOS/lineage-sdk

sdk: Replace los versioning
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

sdk/src/java/lineageos/os/Build.java
@@ -28,10 +28,10 @@ public class Build {
     public static final String UNKNOWN = "unknown";
 
     /** A build ID utilized to distinguish lineageos versions */
-    public static final String LINEAGEOS_VERSION = getString("ro.lineage.version");
+    public static final String PALYRIM_VERSION = getString("ro.palyrim.version");
 
     /** A build ID string meant for displaying to the user */
-    public static final String LINEAGEOS_DISPLAY_VERSION = getString("ro.lineage.display.version");
+    public static final String PALYRIM_DISPLAY_VERSION = getString("ro.palyrim.display.version");
 
     private static final SparseArray<String> sdkMap;
     static

giste/beerdroid_lineage-sdk

sdk/src/java/lineageos/os/Build.java
@@ -28,10 +28,10 @@ public class Build {
     public static final String UNKNOWN = "unknown";
 
     /** A build ID utilized to distinguish lineageos versions */
-    public static final String LINEAGEOS_VERSION = getString("ro.lineage.version");
+    public static final String CRDROID_VERSION = getString("ro.crdroid.version");
 
     /** A build ID string meant for displaying to the user */
-    public static final String LINEAGEOS_DISPLAY_VERSION = getString("ro.lineage.display.version");
+    public static final String CRDROID_DISPLAY_VERSION = getString("ro.crdroid.display.version");
 
     private static final SparseArray<String> sdkMap;
     static